854814506adf9722cd21b4c0616974e20c723336
jongwon.Yoo <ryu01410@gmail.com>
2020-05-06 15:09:02 +0900

    Test.java 두줄주석

--- Test.java
+++ Test.java
@@ -1,1 +1,9 @@
+class Test{
+
+public static void main(String[] args){
+
+System.out.println("Test");
+
+}
+}
 
---
+++ Test4.java
@@ -0,0 +1,6 @@
+class Test4{
+	public static void main(String[] args){
+	System.out.println("test4");
+}
+}
+
